cd244343bea607fee737981dd4cca1b15ae1657f
Ihor <ihor.bilobran@gmail.com>
2024-06-13 18:12:12 +0300

    default angular UI cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'cs-stepper'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('cs-stepper');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cs-stepper app is running!');
   });
 });
